14ac947438c15d0326f100d2621ae2980a170fb9
Fabien Labarbe-Desvignes <fabien.labarbe-desvignes@covergo.com>
2022-12-04 19:06:09 +0800

    day3

---
+++ day3/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+func ex1(ss []string) int {
+	sum := 0
+	for _, s := range ss {
+		var tmp []byte
+		for idx := 0; idx < len(s)/2; idx++ {
+			for idx2 := len(s) / 2; idx2 < len(s); idx2++ {
+				if s[idx] == s[idx2] {
+					if strings.Contains(string(tmp), string(s[idx])) {
+						continue
+					} else {
+						if s[idx] >= 'a' {
+							sum += int(s[idx]-'a') + 1
+						} else {
+							sum += int(s[idx]-'A') + 1 + 26
+						}
+						tmp = append(tmp, s[idx])
+					}
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func getMatches(s1, s2 string) []byte {
+	var matches []byte
+
+	for idx := 0; idx < len(s1); idx++ {
+		for idx2 := 0; idx2 < len(s2); idx2++ {
+			if s1[idx] == s2[idx2] {
+				if strings.Contains(string(matches), string(s2[idx2])) {
+					continue
+				} else {
+					matches = append(matches, s2[idx2])
+				}
+			}
+		}
+	}
+	return matches
+}
+
+func match(s1, s2, s3 string) int {
+	m1 := getMatches(s1, s2)
+	m2 := getMatches(s1, s3)
+	m3 := getMatches(string(m1), string(m2))
+	if m3[0] >= 'a' {
+		return int(m3[0]-'a') + 1
+	} else {
+		return int(m3[0]-'A') + 1 + 26
+	}
+}
+
+func ex2(ss []string) int {
+	sum := 0
+	for idx0 := 0; idx0 < len(ss); idx0 += 3 {
+		//small speed optimization
+		if len(ss[idx0]) <= len(ss[idx0+1]) && len(ss[idx0]) <= len(ss[idx0+2]) {
+			sum += match(ss[idx0], ss[idx0+1], ss[idx0+2])
+		} else if len(ss[idx0+1]) <= len(ss[idx0]) && len(ss[idx0+1]) <= len(ss[idx0+2]) {
+			sum += match(ss[idx0+1], ss[idx0], ss[idx0+2])
+		} else {
+			sum += match(ss[idx0+2], ss[idx0], ss[idx0+1])
+		}
+	}
+	return sum
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	data, _ := io.ReadAll(f)
+	ss := strings.Split(string(data), "\n")
+
+	t := time.Now()
+	fmt.Println(ex1(ss))
+	fmt.Println(time.Since(t))
+
+	t = time.Now()
+	fmt.Println(ex2(ss))
+	fmt.Println(time.Since(t))
+
+}
---
+++ day3/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func benchmarkSolutionEx1(b *testing.B) {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		b.Fatalf("failed to read input: %s", err)
+	}
+	tokens := strings.Split(string(input), "\n")
+	b.Run("solution", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			ex1(tokens)
+		}
+	})
+}
+
+func benchmarkSolutionEx2(b *testing.B) {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		b.Fatalf("failed to read input: %s", err)
+	}
+	tokens := strings.Split(string(input), "\n")
+	b.Run("solution", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			ex2(tokens)
+		}
+	})
+}
+
+func BenchmarkSolutionTopk3OEx1(b *testing.B) { benchmarkSolutionEx1(b) }
+func BenchmarkSolutionTopk3OEx2(b *testing.B) { benchmarkSolutionEx2(b) }
