033fed4ccf8b56b5fbe37e22b746fbe05a7385f4
Fabien Labarbe-Desvignes <fabien.labarbe-desvignes@covergo.com>
2022-12-08 22:46:41 +0800

    day7

---
+++ day7/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var tot = 0
+var sizes = []int{0}
+
+func cd(ss []string, idx int) (int, int) {
+	var s int
+	localSize := 0
+	var subSizes []int
+	subdirs := 0
+	for i := idx; i < len(ss); i++ {
+		cmd := strings.Split(ss[i], " ")
+		if val, err := strconv.Atoi(cmd[0]); err == nil {
+			localSize += val
+			continue
+		} else if string(cmd[1]) == "cd" {
+			if cmd[2] == ".." {
+				return localSize, i
+			}
+			s, i = cd(ss, i+1)
+			subSizes = append(subSizes, s)
+			subdirs--
+			localSize += s
+			if localSize <= 100000 && subdirs == 0 {
+				tot += localSize
+			} else if localSize > 100000 && subdirs == 0 {
+				for _, n := range subSizes {
+					if n < 100000 {
+						tot += n
+					}
+				}
+			}
+			continue
+		} else if cmd[0] == "dir" {
+			subdirs++
+		}
+	}
+	return localSize, len(ss)
+}
+
+func ex1(ss []string) int {
+	cd(ss, 0)
+	return tot
+}
+
+func cd2(ss []string, idx int) (int, int) {
+	var s int
+	localSize := 0
+	var subSizes []int
+	subdirs := 0
+	for i := idx; i < len(ss); i++ {
+		cmd := strings.Split(ss[i], " ")
+		if val, err := strconv.Atoi(cmd[0]); err == nil {
+			localSize += val
+			continue
+		} else if string(cmd[1]) == "cd" {
+			if cmd[2] == ".." {
+				sizes = append(sizes, localSize)
+				return localSize, i
+			}
+			s, i = cd2(ss, i+1)
+			subSizes = append(subSizes, s)
+			subdirs--
+			localSize += s
+			continue
+		} else if cmd[0] == "dir" {
+			subdirs++
+		}
+	}
+	sizes = append(sizes, localSize)
+	return localSize, len(ss)
+}
+
+func ex2(ss []string) int {
+	best := math.MaxInt
+	totSize, _ := cd2(ss, 0)
+	for _, n := range sizes {
+		tmp := 70000000 - (totSize) + n
+		if tmp > 30000000 && n < best {
+			best = n
+		}
+	}
+	return best
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	data, _ := io.ReadAll(f)
+	ss := strings.Split(string(data), "\n")
+
+	t := time.Now()
+	fmt.Println(ex1(ss))
+	fmt.Println(time.Since(t))
+	t = time.Now()
+	fmt.Println(ex2(ss))
+	fmt.Println(time.Since(t))
+}
